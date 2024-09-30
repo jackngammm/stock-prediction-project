a4d4b74d2c1ec09ec32a2f9c219011b30ffaa9b5
jackn2023F <jackn@vt.edu>
2024-09-30 17:32:33 -0400

    plot file

--- plotter.py
+++ plotter.py
@@ -1,14 +1,23 @@
 import matplotlib.pyplot as plt
 
 
-def plot_graph():
+def plot_graph(stock_data, stock_symbol):
+
+    if stock_data is not None and not stock_data.empty:
+        plt.figure(figsize=(10,5))
+        plt.plot(stock_data.index, stock_data['Close'], label = 'Closing Price', market='o')
+        plt.title(f'{stock_symbol} Stock Price ({stock_data.index.min().date()} to {stock_data.index.max().date()})')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+    else:
+        print(f"No data avaialble for {stock_symbol} in the selected date range.")
 
 
 
 
 
 
-def plot():
 
 
 
